35a185cdbc03f4988ea75568067ffa7bfdd05b44
Viwu <sinconsistencia@gmail.com>
2025-09-28 21:35:10 -0500

    parseStateOverwrites base: an array of the text files content

--- source/sphis/textvar/Textvar.hx
+++ source/sphis/textvar/Textvar.hx
@@ -1,6 +1,14 @@
 package sphis.textvar;
 
+import lime.utils.Assets;
+
 class Textvar
 {
 	public static var states:Map<String, Map<String, Dynamic>> = [];
+	public static function parseStateOverwrites(state:String, fileName:String)
+	{
+		var file = Assets.getText('assets/data/$fileName.txt').split('\n');
+
+		for (line in file) {}
+	}
 }
