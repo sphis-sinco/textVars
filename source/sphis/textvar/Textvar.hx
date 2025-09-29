c0ae1578053c56a90c1fa1962345b791a6c09c47
Viwu <sinconsistencia@gmail.com>
2025-09-28 21:52:53 -0500

    value parsing

--- source/sphis/textvar/Textvar.hx
+++ source/sphis/textvar/Textvar.hx
@@ -22,10 +22,38 @@ class Textvar
 			var variable = lineSplit[1];
 			var newValue = lineSplit[2];
 
+			if (file.length > 3)
+			{
+				for (i in 0...file.length)
+				{
+					if (i > 2)
+						newValue += file[i];
+				}
+			}
+
 			if (targetState == state)
 			{
 				var stateMap = states.get(state);
-				stateMap.set(variable, newValue);
+				var type_string = "abcdefghijklmnopqrstuvwxyz`~!@#$%^&*()-=_+[]\\;',/{}|:\"<>? ";
+				var is_string:Bool = false;
+
+				for (i in 0...type_string.length)
+				{
+					var letter = type_string.charAt(i);
+
+					if (StringTools.contains(newValue, letter))
+					{
+						is_string = true;
+						return;
+					}
+				}
+
+				if (is_string)
+					stateMap.set(variable, Std.string(newValue));
+				else
+				{
+					stateMap.set(variable, Std.parseFloat(newValue));
+				}
 			}
 		}
 	}
