cf9296bc0975b57b841db0a04f25a4f373787b40
Viwu <sinconsistencia@gmail.com>
2025-09-28 21:31:35 -0500

    state state

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -8,6 +8,9 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
+		Parser.states.set('state', []);
+		Parser.states.get('state').set('value', -10);
+
 	}
 
 	override public function update(elapsed:Float)
